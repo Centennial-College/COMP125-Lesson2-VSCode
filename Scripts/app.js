14906ec1ba7f5bdd43ef80ca0d21eebe18c1a64f
Kb-Ma <kbma.kevin@gmail.com>
2016-05-17 19:48:30 -0400

    Initial Commit

---
+++ Scripts/app.js
@@ -0,0 +1,12 @@
+/* main JavaScript file */
+
+// IIFE - Immediately Invoked Function Expression
+(function(){
+    "use strict";   // executionable command
+    // don't let me assign variables like a string to an integer
+    
+    // code goes in here
+    
+    console.log("App Started...");
+    
+})();
