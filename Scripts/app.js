687e0295f961ac613a4682a2884a4f09da1ef735
Kb-Ma <kbma.kevin@gmail.com>
2016-05-18 18:34:45 -0400

    updated app.css and app.js

--- Scripts/app.js
+++ Scripts/app.js
@@ -5,39 +5,72 @@
     "use strict";   // executionable command
     // don't let me assign variables like a string to an integer
 
-    // code goes in here
+    var firstPTag = document.getElementById("firstParagraph");
 
+    // event listener click
+    firstPTag.addEventListener("click", firstParagraphClicked);
+    
+    // event listener hover
+    firstPTag.addEventListener("mouseover",firstParagraphHover);
+
+    var firstParagraphIsClicked = false;
+    var firstParagraphWasHovered = false;
+
+    // code goes in here
     console.log("App Started...");
 
-    /*
+    var myFirstParagraph = "This is my first paragraph. I am writing this for demonstration purposes. " +
+        "This is the third sentence of my first paragraph."
+
     // declare named function
     function writeToFirstParagraph() {
         // for debugging only
         console.log("Exexcuting writeToFirstParagraph");
 
         // create a reference to a p element with an id of "firstParagraph"
-        var firstParagraph = document.getElementById("firstParagraph");
 
-        firstParagraph.textContent = "It's alive!!!";
-        
-        console.log(tom);
+        firstPTag.textContent = myFirstParagraph;
     }
+
+    // event handler for firstParagraph clicked
+    function firstParagraphClicked() {
+        if (firstParagraphIsClicked) {
+            firstPTag.style.color = "#000000";
+            firstParagraphIsClicked = false;
+        }
+        else {
+            firstPTag.style.color = "#ff0000";
+            firstParagraphIsClicked = true;
+        }
+    }
+
+    // event handler for firstParagraph hover
+    function firstParagraphHover() {
+        if (firstParagraphWasHovered) {
+            firstPTag.style.color = "#000000";
+            firstParagraphWasHovered = false;
+        }
+        else {
+            firstPTag.style.color = "#00ff00";
+            firstParagraphWasHovered = true;
+        }
+    }
+
+    /*
+        // declare a variable that points to anonymous function
+        var writeToFirstParagraph = function () {
+            // for debugging only
+            console.log("Exexcuting writeToFirstParagraph");
+    
+            // create a reference to a p element with an id of "firstParagraph"
+            var firstPTag = document.getElementById("firstParagraph");
+    
+            firstPTag.textContent = "It's alive!!!";
+        }
     */
-
-    // declare a variable that points to anonymous function
-    var writeToFirstParagraph = function () {
-        // for debugging only
-        console.log("Exexcuting writeToFirstParagraph");
-
-        // create a reference to a p element with an id of "firstParagraph"
-        var firstPTag = document.getElementById("firstParagraph");
-
-        firstPTag.textContent = "It's alive!!!";
-    }
-
     // execute function
     writeToFirstParagraph();
-    
-    console.log(firstPTag);
+
+    //console.log(firstPTag);
 
 })();
