84a26ae4af6c34677d04cbb80a5c45a6e1f25e2f
Kb-Ma <kbma.kevin@gmail.com>
2016-05-18 17:42:19 -0400

    updated app.js

--- Scripts/app.js
+++ Scripts/app.js
@@ -9,9 +9,35 @@
 
     console.log("App Started...");
 
-    // create a reference to a p element with an id of "firstParagraph"
-    var firstParagraph = document.getElementById("firstParagraph");
+    /*
+    // declare named function
+    function writeToFirstParagraph() {
+        // for debugging only
+        console.log("Exexcuting writeToFirstParagraph");
 
-    firstParagraph.textContent = "It's alive!!!";
+        // create a reference to a p element with an id of "firstParagraph"
+        var firstParagraph = document.getElementById("firstParagraph");
+
+        firstParagraph.textContent = "It's alive!!!";
+        
+        console.log(tom);
+    }
+    */
+
+    // declare a variable that points to anonymous function
+    var writeToFirstParagraph = function () {
+        // for debugging only
+        console.log("Exexcuting writeToFirstParagraph");
+
+        // create a reference to a p element with an id of "firstParagraph"
+        var firstPTag = document.getElementById("firstParagraph");
+
+        firstPTag.textContent = "It's alive!!!";
+    }
+
+    // execute function
+    writeToFirstParagraph();
+    
+    console.log(firstPTag);
 
 })();
