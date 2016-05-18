88bf74b81b4f1a47b5369bbe54f201886c1a1011
Kb-Ma <kbma.kevin@gmail.com>
2016-05-17 21:40:36 -0400

    added simple code to app.js

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,12 +1,17 @@
 /* main JavaScript file */
 
 // IIFE - Immediately Invoked Function Expression
-(function(){
+(function () {
     "use strict";   // executionable command
     // don't let me assign variables like a string to an integer
-    
+
     // code goes in here
-    
+
     console.log("App Started...");
-    
+
+    // create a reference to a p element with an id of "firstParagraph"
+    var firstParagraph = document.getElementById("firstParagraph");
+
+    firstParagraph.textContent = "It's alive!!!";
+
 })();
